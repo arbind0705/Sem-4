a9bf6947072a8f7bf077b5c4a71c3357aea2ad94
Arbind <arbindmalava@gmail.com>
2025-04-10 19:24:58 +0530

    Update Vite config to use relative paths for assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Use relative paths for assets
   root: 'src', // Set the root directory where Vite should look for files
   build: {
     outDir: '../dist', // Output directory after building the project
